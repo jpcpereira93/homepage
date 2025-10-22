211e9b62b10f8bd01018a0992389564fee130b1d
jpcpereira93 <jpcpereira93@gmail.com>
2025-10-22 02:41:39 +0100

    feat(home): display particles on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { Particles } from "@/components";
+
 export default function Home() {
   return (
     <main className="w-screen h-screen flex flex-col justify-center items-center gap-2">
+      <Particles />
       <h1 className="text-5xl font-light text-slate-200 animate-move-in-left">
         Hello, I'm <span className="text-teal-400 font-normal">José</span>.
       </h1>
---
+++ src/components/Particles/index.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import type { Container, ISourceOptions } from "@tsparticles/engine";
+import ParticlesExternal, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+import { useEffect, useMemo, useState } from "react";
+
+export const Particles = () => {
+  const options = useMemo<ISourceOptions>(
+    () => ({
+      detectRetina: true,
+      fullScreen: {
+        enable: true,
+        zIndex: -1,
+      },
+      fpsLimit: 120,
+      particles: {
+        number: {
+          value: 70,
+          density: {
+            enable: false,
+          },
+        },
+        color: {
+          value: ["#00d5be", "#00bcff", "#00d5be"],
+        },
+        shape: {
+          type: "circle",
+        },
+        opacity: {
+          value: 0.3,
+        },
+        size: {
+          value: { min: 1, max: 2 },
+        },
+        links: {
+          blink: false,
+          enable: true,
+          distance: 80,
+          color: "#00d5be",
+          opacity: 0.1,
+          width: 1,
+        },
+        move: {
+          enable: true,
+          speed: 1,
+          outModes: {
+            default: "out",
+          },
+        },
+      },
+      interactivity: {
+        events: {
+          onHover: {
+            enable: true,
+            mode: "attract",
+          },
+        },
+        modes: {
+          attract: {
+            distance: 250,
+            duration: 0.4,
+            easing: "ease-out-quad",
+            factor: 1,
+            maxSpeed: 50,
+            speed: 0.5,
+          },
+        },
+      },
+    }),
+    [],
+  );
+
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = async (container?: Container) => {
+    console.log(container);
+  };
+
+  if (init) {
+    return (
+      <ParticlesExternal
+        id="particles"
+        particlesLoaded={particlesLoaded}
+        options={options}
+      />
+    );
+  }
+
+  return <></>;
+};
