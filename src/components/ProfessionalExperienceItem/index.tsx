dc3217d500cb579ce93228e6d3c910be6049d303
jpcpereira93 <jpcpereira93@gmail.com>
2025-10-23 19:58:46 +0100

    fix(experience): align items content

--- src/components/ProfessionalExperienceItem/index.tsx
+++ src/components/ProfessionalExperienceItem/index.tsx
@@ -19,12 +19,12 @@ export const ProfessionalExperienceItem = ({
 }: ProfessionalExperienceItemProps) => {
   return (
     <article className="group flex flex-col lg:flex-row px-10 py-5 gap-2 lg:gap-8 rounded-xl hover:bg-neutral-800/50">
-      <div className="h-10 w-200 flex items-center">
+      <div className="h-10 w-50 flex items-center">
         <p className="uppercase text-sm font-medium tracking-tight text-slate-500">
           {startDate} - {endDate}
         </p>
       </div>
-      <div className="flex flex-col gap-4">
+      <div className="w-full flex flex-col flex-wrap gap-4">
         <div className="flex flex-col">
           <div className="h-10 flex items-center">
             <h5 className="text-lg font-medium tracking-tight text-slate-300 group-hover:text-teal-300">
