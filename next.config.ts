9c563f9bf771bced1e229fe7d19a69253376dfce
jpcpereira93 <jpcpereira93@gmail.com>
2025-10-23 21:55:00 +0100

    fix: change images to unoptimized

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: process.env.NODE_ENV === "production" ? "export" : undefined,
   basePath: process.env.PAGES_BASE_PATH,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
