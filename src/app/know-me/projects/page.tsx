1282e8ffffb37e8dd8c5c1f30d0feb1b3006ab47
jpcpereira93 <jpcpereira93@gmail.com>
2025-11-04 04:01:30 +0000

    feat(projects): add personal homepage project

--- src/app/know-me/projects/page.tsx
+++ src/app/know-me/projects/page.tsx
@@ -10,11 +10,18 @@ const PROJECTS: IProjectItem[] = [
     name: "FeatherPlay",
     url: "https://jpcpereira93.github.io/feather-play/",
   },
+  {
+    description:
+      "A modern, production-ready personal webpage. Made with Next.js and TailwindCSS.",
+    img: getImagePath("/img/personal-homepage.png"),
+    name: "Personal Homepage",
+    url: "https://github.com/jpcpereira93/homepage",
+  },
 ];
 
 export default function Projects() {
   return (
-    <ul className="h-full xl:overflow-scroll px-5 sm:px-30 xl:pl-0">
+    <ul className="h-full w-full xl:overflow-scroll px-5 sm:px-30 xl:pl-0 gap-6 flex flex-col">
       {PROJECTS.map(({ description, img, name, url }) => (
         <ProjectItem
           description={description}
--- src/components/ProjectItem/index.tsx
+++ src/components/ProjectItem/index.tsx
@@ -12,7 +12,7 @@ interface ProjectItemProps {
 const ProjectItem = ({ description, img, name, url }: ProjectItemProps) => {
   return (
     <a href={url} target="_blank" rel="noopener noreferrer">
-      <div className="group relative h-50 w-full rounded-lg p-4 flex hover:bg-zinc-800/40 hover:cursor-pointer">
+      <div className="group relative h-60 w-full rounded-lg p-4 gap-4 flex items-center  hover:bg-zinc-800/40 hover:cursor-pointer animate-move-in-right">
         <button
           className="absolute top-4 right-4 text-teal-400 hover:cursor-pointer"
           type="button"
@@ -20,14 +20,14 @@ const ProjectItem = ({ description, img, name, url }: ProjectItemProps) => {
           <SquareArrowOutUpRight size={18} />
         </button>
 
-        <div className="relative w-60 h-full rounded-lg overflow-hidden">
-          <Image alt={name} src={img} priority fill />
+        <div className="relative min-w-30 max-w-30 lg:min-w-40 lg:max-w-40 xl:min-w-50 xl:max-w-50 h-30 lg:h-40 xl:h-50 rounded-lg overflow-hidden">
+          <Image alt={name} objectFit="cover" src={img} priority fill />
         </div>
         <div className="flex flex-col p-4 gap-2">
           <h1 className="text-3xl text-slate-200 font-bold group-hover:text-teal-500">
             {name}
           </h1>
-          <p className="text-sm whitespace-pre-line text-justify">
+          <p className="text-sm whitespace-pre-line text-justify overflow-hidden truncate">
             {description}
           </p>
         </div>
